1de1141aa6e63ba9de59dacf32a92634c1ae0c35
jurtasunel <114501562+jurtasunel@users.noreply.github.com>
2024-09-24 16:20:49 +0100

    Update ROC2024.py

--- FoB/ROC2024.py
+++ FoB/ROC2024.py
@@ -3,16 +3,16 @@ import matplotlib.pyplot as plt
 
 # Make a list of lists with predictions and labels.
 data = []
-ground_truth = ["Benign", "Benign", "Pathogenic", "Pathogenic", "Benign"]
-impact_prediction = [0.1, 0.4, 0.35, 0.9, 0.7] 
+ground_truth = ["Benign", "Benign", "Pathogenic", "Pathogenic", "Benign", "Pathogenic"]
+impact_prediction = [0.1, 0.4, 0.35, 0.8, 0.9, 0.7] 
 for i in range(len(ground_truth)):
     data.append([impact_prediction[i], ground_truth[i]])
 
 # Sort the list by the impact prediction in descending order.
 data.sort(reverse=True, key=lambda x: x[0])
-
+print(data)
 # Initialize variables to store tpr and fpr coordinates.
-tpr_values, fpr_values = [], []
+tpr_values, fpr_values = [0], [0]
 
 # Count the total number of Pathogenic and Benign cases.
 total_P = ground_truth.count("Pathogenic")
@@ -36,5 +36,7 @@ for pair in data:
     fpr_values.append(FPR)
 
 # Plot ROC curve.
-plt.plot(fpr_values, tpr_values)
+plt.plot(fpr_values, tpr_values, marker = "o")
+plt.xlabel('FPR')
+plt.ylabel('TPR')
 plt.show()
