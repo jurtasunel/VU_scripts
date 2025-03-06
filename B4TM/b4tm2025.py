95586dbf731ab4d66aa9d44d7b120e6f6f591395
jurtasunel <114501562+jurtasunel@users.noreply.github.com>
2025-03-06 22:18:25 +0100

    Update b4tm2025.py

--- B4TM/b4tm2025.py
+++ B4TM/b4tm2025.py
@@ -14,9 +14,21 @@ import pandas as pd
 ### Functions.
 
 ### Data.
-os.chdir("/mnt/c/Users/zir826/Desktop/2024-25/B4TM/Practicals/")
+os.chdir("/home/josemari/Desktop/MasterVU/2024/B4TM/")
 train_call = pd.read_csv("Train_call.txt", sep = "\t")
 train_clinical = pd.read_csv("Train_clinical.txt", sep = "\t")
 
-print(train_call)
-print(train_clinical)
+### Data processing.
+# Separate metadata from features, and transpose features to get them as columns.
+metadata = train_call[train_call.columns[0:4]]
+features = train_call.drop(columns = train_call.columns[0:4])
+features = features.T
+
+# Set the samples as index (rownames) of the labels dataset to join them to the features dataframe.
+train_clinical = train_clinical.set_index(train_clinical.columns[0])
+features = train_clinical.join(features, how = "right")
+print(features)
+ 
+# print(features.loc[["Array.10", "Array.101", "Array.102", "Array.104", "Array.105"]].iloc[:,:11])
+# print(features.index.tolist())
+# print(train_clinical[train_clinical.columns[0]])
