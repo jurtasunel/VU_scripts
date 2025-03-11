0290e71adeb79792928433c9d10fbc277bb5335b
jurtasunel <114501562+jurtasunel@users.noreply.github.com>
2025-03-11 07:33:04 +0100

    Update b4tm2025.R

--- B4TM/b4tm2025.R
+++ B4TM/b4tm2025.R
@@ -3,7 +3,7 @@
 ### COURSE CODE: X_405092                             ###
 ### PRACTICAL: Tumor type Predicion                   ###
 ### AUTHOR: Josemari Urtasun Elizari                  ###
-### LAST MODIFIED: 06/03/2024                         ###
+### LAST MODIFIED: 10/03/2024                         ###
 #########################################################
 
 ### Documentation:
@@ -16,11 +16,14 @@ library(caret)
 
 
 ### Data.
-setwd("C:/Users/zir826/Desktop/2024-25/B4TM/Practicals")
+setwd("/home/josemari/Desktop/MasterVU/2024/B4TM")
 train_call <- read.delim("Train_call.txt")
 train_clinical <- read.delim("Train_clinical.txt")
 
-### Processing.
+###################
+### Processing. ###
+###################
+
 # Remove metadata and transpose the matrix to have features as columns.
 features <- train_call[,5:ncol(train_call)]
 features <- as.data.frame(t(as.matrix(features)))
@@ -34,7 +37,12 @@ combdata <- merge(train_clinical, features, by = "row.names")
 rownames(combdata) <- combdata$Row.names
 combdata$Row.names <- NULL
 
-### Feature selection.
+
+
+##########################
+### Feature selection. ###
+##########################
+
 # Separate features and labels.
 features <- combdata[,-1]
 labels <- factor(combdata$Subgroup)
@@ -47,22 +55,41 @@ rocVarImp$average <- averageImp
 # Sort in decreasing order to get the most important features.
 rocVarImp <- rocVarImp[order(rocVarImp$average, decreasing = TRUE),]
 
-### Check loadings with different method.
-pca_results <- prcomp(features, scale. = TRUE, center = TRUE)
-# Get the importance of each pc.
-pc_importace <- summary(pca_results)$importance
-# Make scree plot to visualize the proportion of variance from each component.
-plot(pc_importace[2,], type = "b", col = "darkblue", ylab = "Proportion of Variance", xlab = "PC components", main = "Scree plot")
-
-# Get the loadings (the contribution of each feature to each pc).
-pc_loadings <- pca_results$rotation
-
-# Examine the correlation between variables.
-correl_features <- cor(features)
-#heatmap(correl_features, main = "Feature Correlation", col = colorRampPalette(c("blue", "white", "red"))(20), margins = c(10, 10))
+# Keep features with importance => 0.7.
+idx_rows <- which(rocVarImp$average >= 0.7) # Alternatively, just keep the top 20 features.
+important_features <- rownames(rocVarImp)[idx_rows]
+filter_data <- combdata[, c("Subgroup", important_features)]
 
 
+############
+### KNN. ###
+############
+
+knn_model <- train(x = filter_data[,2:ncol(filter_data)],
+                   y = filter_data$Subgroup,
+                   method = "knn",
+                   tuneLength = 10,
+                   trControl = trainControl(method = "CV", 10),
+                   
+                   )
 
 
+print(knn_model)
+# 
+# ### Check loadings with different method.
+# pca_results <- prcomp(features, scale. = TRUE, center = TRUE)
+# # Get the importance of each pc.
+# pc_importace <- summary(pca_results)$importance
+# # Make scree plot to visualize the proportion of variance from each component.
+# plot(pc_importace[2,], type = "b", col = "darkblue", ylab = "Proportion of Variance", xlab = "PC components", main = "Scree plot")
+# 
+# # Get the loadings (the contribution of each feature to each pc).
+# pc_loadings <- pca_results$rotation
+# 
+# # Examine the correlation between variables.
+# correl_features <- cor(features)
+# #heatmap(correl_features, main = "Feature Correlation", col = colorRampPalette(c("blue", "white", "red"))(20), margins = c(10, 10))
+# 
+# 
 
 
